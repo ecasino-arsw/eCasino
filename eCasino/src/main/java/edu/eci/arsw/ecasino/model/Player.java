7e60c0bf9211762f4a064d531aa83f85cb852150
Daniel Vela <de.vela.lopez@gmail.com>
2019-03-13 11:05:38 -0500

    Added player empty class and updated model

---
+++ eCasino/src/main/java/edu/eci/arsw/ecasino/model/Player.java
@@ -0,0 +1,19 @@
+package edu.eci.arsw.ecasino.model;
+
+public class Player {
+	
+	private String username;
+	
+	public Player() {
+		
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+}
--- eCasino/src/main/java/edu/eci/arsw/ecasino/model/Table.java
+++ eCasino/src/main/java/edu/eci/arsw/ecasino/model/Table.java
@@ -1,11 +1,14 @@
 package edu.eci.arsw.ecasino.model;
 
+import java.util.ArrayList;
+
 public class Table {
 	
 	private Game game;
+	private ArrayList<Player> players;
 	
 	public Table() {
-		
+		players = new ArrayList<>();
 	}
 	
 	public Table(Game game) {
@@ -18,6 +21,14 @@ public class Table {
 
 	public void setGame(Game game) {
 		this.game = game;
+	}
+
+	public ArrayList<Player> getPlayers() {
+		return players;
+	}
+
+	public void setPlayers(ArrayList<Player> players) {
+		this.players = players;
 	}
 
 }
