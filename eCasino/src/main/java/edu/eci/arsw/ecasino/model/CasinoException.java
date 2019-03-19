782417444221a24f2456041322256927c10db5b0
Daniel Vela <de.vela.lopez@gmail.com>
2019-03-19 10:21:30 -0500

    Added basic currency logic to players

--- eCasino/src/main/java/edu/eci/arsw/ecasino/model/CasinoException.java
+++ eCasino/src/main/java/edu/eci/arsw/ecasino/model/CasinoException.java
@@ -1,5 +1,18 @@
 package edu.eci.arsw.ecasino.model;
 
-public class CasinoException {
+public class CasinoException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7578614015362648167L;
+
+	public CasinoException(String message) {
+        super(message);
+    }
+
+    public CasinoException(String message, Throwable cause) {
+        super(message, cause);
+    } 
 
 }
