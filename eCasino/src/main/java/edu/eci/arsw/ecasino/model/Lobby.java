c15a3544075638a1e0c324c7ff29969d22c1dbb3
dcifuentesr <danixbox1313@gmail.com>
2019-03-19 05:08:53 -0500

    join lobby/create table 1

---
+++ eCasino/src/main/java/edu/eci/arsw/ecasino/model/Lobby.java
@@ -0,0 +1,47 @@
+package edu.eci.arsw.ecasino.model;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Set;
+
+public class Lobby {
+	
+	private int id;
+	private Map<Integer,Table> tables;
+	private Set<Player> players;
+	
+	public Lobby(int id) {
+		this.id = id;
+		tables = new HashMap<Integer,Table>();
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public Map<Integer, Table> getTables() {
+		return tables;
+	}
+
+	public void setTables(Map<Integer, Table> tables) {
+		this.tables = tables;
+	}
+	
+	public Table getTable(int id) {
+		return tables.get(id);
+	}
+	
+	public void addTable(Table table) {
+		tables.put(table.getId(), table);
+	}
+	
+	public void addPlayer(Player player) {
+		players.add(player);
+	}
+
+}
