c517ae69dee7f1b29ce517312eed0877c5b2ca29
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2019-03-26 05:39:35 -0500

    Implement lobby and Account

--- src/main/java/edu/eci/arsw/ecasino/model/Table.java
+++ src/main/java/edu/eci/arsw/ecasino/model/Table.java
@@ -5,44 +5,52 @@ import java.util.Set;
 import edu.eci.arsw.ecasino.model.game.Game;
 
 public class Table {
-	
-	private int id;
-	private Game game;
-	private Set<Player> players;
-	
-	
 
-	public Table(int id,Game game) {
-		this.id=id;
-		this.game=game;
-	}
-	
-	public Table(Game game) {
-		setGame(game);
-	}
+    private int id;
+    private String nametable;
+    private Game game;
+    private Set<Player> players;
 
-	public int getId() {
-		return id;
-	}
+    public Table(int id, String nametable, Game game) {
+        this.id = id;
+        this.nametable = nametable;
+        this.game = game;
+    }
 
-	public void setId(int id) {
-		this.id = id;
-	}
+    public String getNameTable() {
+        return nametable;
+    }
 
-	public Game getGame() {
-		return game;
-	}
+    public void setNameTable(String nametable) {
+        this.nametable = nametable;
+    }
 
-	public void setGame(Game game) {
-		this.game = game;
-	}
-	
-	public void addPlayer(Player player) {
-		players.add(player);
-	}
-	
-	public Set<Player> getPlayers() {
-		return players;
-	}
+    public Table(Game game) {
+        setGame(game);
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Game getGame() {
+        return game;
+    }
+
+    public void setGame(Game game) {
+        this.game = game;
+    }
+
+    public void addPlayer(Player player) {
+        players.add(player);
+    }
+
+    public Set<Player> getPlayers() {
+        return players;
+    }
 
 }
