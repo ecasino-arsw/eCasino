74af2fd2db59a2a219f7a9e650ff26e027b8eeda
Daniel Vela <de.vela.lopez@gmail.com>
2019-03-27 15:05:48 -0500

    Added request controller in main application

--- src/main/java/edu/eci/arsw/ecasino/CasinoAPIApplication.java
+++ src/main/java/edu/eci/arsw/ecasino/CasinoAPIApplication.java
@@ -3,11 +3,14 @@ package edu.eci.arsw.ecasino;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 /**
  *
  * @author Daniel Vela 
  */
+@Controller
 @SpringBootApplication
 @ComponentScan(basePackages = {"edu.eci.arsw.ecasino"})
 public class CasinoAPIApplication {
@@ -15,4 +18,10 @@ public class CasinoAPIApplication {
     public static void main(String[] args) {
         SpringApplication.run(CasinoAPIApplication.class, args);
     }
+
+    @RequestMapping("/")
+    String index() {
+        return "index";
+    }
+
 }
