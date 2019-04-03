fafe67ffa74c67cf3b8fc3b337fd23e9336a683b
dcifuentesr <danixbox1313@gmail.com>
2019-04-03 15:06:07 -0500

    update lobbies in memory

--- src/main/java/edu/eci/arsw/ecasino/model/Lobby.java
+++ src/main/java/edu/eci/arsw/ecasino/model/Lobby.java
@@ -2,12 +2,15 @@ package edu.eci.arsw.ecasino.model;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class Lobby {
 
     private int id;
     private Map<Integer, Table> tables;
+    private Set<Player> players;
     private String name;
     private int numOfTables;
 
@@ -15,10 +18,16 @@ public class Lobby {
         this.id = id;
         this.name = name;
         tables = new HashMap<Integer, Table>();
+        players = new HashSet<Player>();
         this.numOfTables = 0;
     }
 
-    public String getName() {
+    private Set<Player> getPlayers() {
+		
+		return players;
+	}
+
+	public String getName() {
         return name;
     }
 
@@ -58,5 +67,17 @@ public class Lobby {
         tables.put(table.getId(), table);
         setNumOfTables();
     }
+
+    public void updateLobby(Lobby lobby) {
+    	name=lobby.getName();
+    	tables = lobby.getTables();
+    	players=lobby.getPlayers();
+    	numOfTables=tables.size();
+    }
+    
+	public void addPlayer(Player player) {
+		players.add(player);
+		
+	}
 
 }
