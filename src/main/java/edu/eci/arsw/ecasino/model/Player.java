ba1d171961de25bbdbe70d23ed28cbd3998ee8b2
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2019-04-08 20:07:06 -0500

    player model changes, connect backend (login and sign up

--- src/main/java/edu/eci/arsw/ecasino/model/Player.java
+++ src/main/java/edu/eci/arsw/ecasino/model/Player.java
@@ -5,59 +5,72 @@ import java.util.UUID;
 
 public class Player implements Serializable {
 
-	private Integer id;
-	private String username;
-	private String password;
-	private String fullName;
-	private Integer money;
-	
-	public Player() {
-		
-	}
-	
-	public Player(Integer id, String username, String password, String fullName, Integer money) {
-		this.id = id;
-		this.username = username;
-		this.password = password;
-		this.fullName = fullName;
-		this.money = money;
-	}
-	
-	public Integer getId() {
-		return id;
-	}
+    private Integer id;
+    private String username;
+    private String password;
+    private String fullName;
+    private String email;
+    private double money;
 
-	public void setId(Integer id) {
-		this.id = id;
-	}
+    public Player() {
 
-	public String getUsername() {
-		return username;
-	}
-	public void setUsername(String username) {
-		this.username = username;
-	}
-	public String getPassword() {
-		return password;
-	}
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public Player(Integer id, String username, String password, String fullName, String email) {
+        this.id = id;
+        this.username = username;
+        this.password = password;
+        this.fullName = fullName;
+        this.email = email;
+        this.money = 0.0;
+    }
 
-	public String getFullName() {
-		return fullName;
-	}
+    public Integer getId() {
+        return id;
+    }
 
-	public void setFullName(String fullName) {
-		this.fullName = fullName;
-	}
+    public void setId(Integer id) {
+        this.id = id;
+    }
 
-	public int getMoney() {
-		return money;
-	}
-	public void setMoney(int money) {
-		this.money = money;
-	}
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public double getMoney() {
+        return money;
+    }
+
+    public void setMoney(float money) {
+        this.money = money;
+    }
 
 }
