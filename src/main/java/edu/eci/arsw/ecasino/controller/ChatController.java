3cee72ef3d1588503099120e063310c1ce33a707
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2019-05-10 22:19:33 -0500

    implement chat in rooms

---
+++ src/main/java/edu/eci/arsw/ecasino/controller/ChatController.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.arsw.ecasino.controller;
+
+import edu.eci.arsw.ecasino.model.ChatMessage;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.DestinationVariable;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.messaging.simp.SimpMessageHeaderAccessor;
+import org.springframework.stereotype.Controller;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+
+/**
+ *
+ * @author 2106991
+ */
+@Controller
+public class ChatController {
+    
+    @Autowired
+    SimpMessagingTemplate msgt;
+
+    
+    @MessageMapping("/topic/public{channel}")
+    @SendTo("/topic/public{channel}")
+    public ChatMessage sendMessage(@DestinationVariable String channel,@Payload ChatMessage chatMessage) {
+        return chatMessage;
+    }
+
+    @MessageMapping("/chat.addUser{channel}")
+    @SendTo("/topic/public{channel}")
+    public ChatMessage addUser(@Payload ChatMessage chatMessage,
+            SimpMessageHeaderAccessor headerAccessor) {
+        // Add username in web socket session
+        headerAccessor.getSessionAttributes().put("username", chatMessage.getSender());
+        return chatMessage;
+    }
+
+}
