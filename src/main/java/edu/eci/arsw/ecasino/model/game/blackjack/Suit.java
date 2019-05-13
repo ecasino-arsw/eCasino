c7c8efa9c53d094dc02b9589d378a190924718c1
Daniel Vela <de.vela.lopez@gmail.com>
2019-05-13 18:53:17 -0500

    Added BlackJack logic (console, single player)

---
+++ src/main/java/edu/eci/arsw/ecasino/model/game/blackjack/Suit.java
@@ -0,0 +1,5 @@
+package edu.eci.arsw.ecasino.model.game.blackjack;
+
+public enum Suit {
+	HEART, SPADE, DIAMOND, CLUB
+}
---
+++ src/main/java/edu/eci/arsw/ecasino/model/game/blackjack/Value.java
@@ -0,0 +1,5 @@
+package edu.eci.arsw.ecasino.model.game.blackjack;
+
+public enum Value {
+	TWO, THREE, FOUR, FIVE, SIX, SEVEN, EIGHT, NINE, TEN, JACK, QUEEN, KING, ACE
+}
