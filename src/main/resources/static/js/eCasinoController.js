68a1d1d8fec188b5eb65cc8ebeb2671500664856
Juan Villate <juan.villate-i@mail.escuelaing.edu.co>
2019-05-14 05:25:58 -0500

    implementation holdEm

--- src/main/resources/static/js/eCasinoController.js
+++ src/main/resources/static/js/eCasinoController.js
@@ -22,7 +22,7 @@ function loadPanel() {
     saveLobbies();
     loadTables();
     loadBalance();
-    window.setInterval("loadTables()",3000);
+    window.setInterval("loadTables()", 3000);
 
 }
 
@@ -30,12 +30,12 @@ function loadPanel() {
 function loadTables() {
     axios.get('/lobbies/tables')
             .then(function (response) {
-                document.getElementById("tablesBlackJack").innerHTML = '<table><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Game</th>'+
-                    '<th scope="col">Persons</th><th scope="col">Stakes</th> <th ></th></tr></table>';
-                document.getElementById("tablesHoldEm").innerHTML = '<table><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Game</th>'+
-                    '<th scope="col">Persons</th><th scope="col">Stakes</th> <th ></th></tr></table>';
-                document.getElementById("tablesRoulette").innerHTML = '<table><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Game</th>'+
-                    '<th scope="col">Persons</th><th scope="col">Stakes</th> <th ></th></tr></table>';
+                document.getElementById("tablesBlackJack").innerHTML = '<table><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Game</th>' +
+                        '<th scope="col">Persons</th><th scope="col">Stakes</th> <th ></th></tr></table>';
+                document.getElementById("tablesHoldEm").innerHTML = '<table><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Game</th>' +
+                        '<th scope="col">Persons</th><th scope="col">Stakes</th> <th ></th></tr></table>';
+                document.getElementById("tablesRoulette").innerHTML = '<table><tr><th scope="col">#</th><th scope="col">Name</th><th scope="col">Game</th>' +
+                        '<th scope="col">Persons</th><th scope="col">Stakes</th> <th ></th></tr></table>';
                 var tableBlack = $("#tablesBlackJack");
                 var tableHoldEm = $("#tablesHoldEm");
                 var tableRoulette = $("#tablesRoulette");
@@ -51,19 +51,19 @@ function loadTables() {
                     if (lobby === "blackJack") {
                         cont = contBlack;
                         var onclick = 'onclick="joinGame(' + selectTable['id'] + ',' + selectTable['lobbyId'] + ')"';
-                        tableBlack.append('<tr><td scope="row">' + cont + "</td><td>" + selectTable['name'] + "</td><td>" + lobby + "</td><td>/"+selectTable['capacity']+"</td><td>" + "$" + selectTable['stakes'] + " USD" + '</td><td><button class="btn btn-info" name="button" ' + onclick + ' >Join</button></td>')
+                        tableBlack.append('<tr><td scope="row">' + cont + "</td><td>" + selectTable['name'] + "</td><td>" + lobby + "</td><td>" + selectTable['currentPlayers'] + "/" + selectTable['capacity'] + "</td><td>" + "$" + selectTable['stakes'] + " USD" + '</td><td><button class="btn btn-info" name="button" ' + onclick + ' >Join</button></td>')
                         contBlack++;
                     }
                     if (lobby === "rulette") {
                         cont = contRoulette;
                         var onclick = 'onclick="joinGame(' + selectTable['id'] + ',' + selectTable['lobbyId'] + ')"';
-                        tableRoulette.append('<tr><td scope="row">' + cont + "</td><td>" + selectTable['name'] + "</td><td>" + lobby + "</td><td>/"+selectTable['capacity']+"</td><td>" + "$" + selectTable['stakes'] + " USD" + '</td><td><button class="btn btn-info" name="button" ' + onclick + ' >Join</button></td>')
+                        tableRoulette.append('<tr><td scope="row">' + cont + "</td><td>" + selectTable['name'] + "</td><td>" + lobby + "</td><td>" + selectTable['currentPlayers'] + "/" + selectTable['capacity'] + "</td><td>" + "$" + selectTable['stakes'] + " USD" + '</td><td><button class="btn btn-info" name="button" ' + onclick + ' >Join</button></td>')
                         contRoulette++;
                     }
                     if (lobby === "holdEm") {
                         cont = contHoldEm;
                         var onclick = 'onclick="joinGame(' + selectTable['id'] + ',' + selectTable['lobbyId'] + ')"';
-                        tableHoldEm.append('<tr><td scope="row">' + cont + "</td><td>" + selectTable['name'] + "</td><td>" + lobby + "</td><td>/"+selectTable['capacity']+"</td><td>" + "$" + selectTable['stakes'] + " USD" + '</td><td><button class="btn btn-info" name="button" ' + onclick + '>Join</button></td>')
+                        tableHoldEm.append('<tr><td scope="row">' + cont + "</td><td>" + selectTable['name'] + "</td><td>" + lobby + "</td><td>" + selectTable['currentPlayers'] + "/" + selectTable['capacity'] + "</td><td>" + "$" + selectTable['stakes'] + " USD" + '</td><td><button class="btn btn-info" name="button" ' + onclick + '>Join</button></td>')
                         contHoldEm++;
                     }
 
@@ -98,6 +98,11 @@ function joinGame(id, lobbyId) {
     axios.get('/lobbies/' + lobbyId + '/tables/' + id)
             .then(function (response) {
                 var table = response.data;
+                var currentPlayers = table['currentPlayers'];
+                localStorage.setItem('playersInTable', currentPlayers);
+                localStorage.setItem('capacityTable', table['capacity']);
+                localStorage.setItem('currentLobbyId', lobbyId)
+                localStorage.setItem('currentStakes', table['stakes'])
                 var nameGame = 'lobby' + table['lobbyId'].toString();
                 var lobby = localStorage.getItem(nameGame);
                 var text = "You are going to enter the table: <b>" + table['name'] + "</b> of the game <b>" + lobby + "</b>";
@@ -109,6 +114,34 @@ function joinGame(id, lobbyId) {
 
 }
 
+function joinPlayerInTable(web) {
+    var capacity = parseInt(localStorage.getItem('capacityTable'));
+    var players = parseInt(localStorage.getItem('playersInTable'));
+
+    console.log(players + "/" + capacity);
+    if (players < capacity) {
+        var players = parseInt(localStorage.getItem('playersInTable')) + 1;
+        
+        console.log(players + "/" + capacity);
+        axios.put('/lobbies/' + localStorage.getItem('currentLobbyId') + '/tables', {
+            id: localStorage.getItem('currentTable') ,
+            lobbyId: localStorage.getItem('currentLobbyId') ,
+            name: localStorage.getItem('currentNameTable') ,
+            stakes: localStorage.getItem('currentStakes') ,
+            capacity: capacity,
+            currentPlayers: players
+        })
+                .then(function (response) {
+                    localStorage.setItem('playersInTable', players);
+                    location.href = "gameholdem.html";
+
+                })
+                .catch(function (error) {
+                    alert("Error, not join in table");
+                })
+    }
+}
+
 function confirmar(text, currentTable, currentLobby, currentNameTable) {
     var confirm = alertify.confirm('ENTERING A ROOM', text, null, null).set('labels', {ok: 'Join', cancel: 'Cancel'});
 
@@ -118,7 +151,15 @@ function confirmar(text, currentTable, currentLobby, currentNameTable) {
         localStorage.setItem('currentTable', currentTable);
         localStorage.setItem('currentLobby', currentLobby);
         localStorage.setItem('currentNameTable', currentNameTable);
-        location.href = "game.html";
+        if (currentLobby === 'rulette') {
+            joinPlayerInTable(location.href = "gameroulette.html");
+        } else if (currentLobby === 'holdEm') {
+            joinPlayerInTable(location.href = "gameholdem.html");
+            //location.href = "gameholdem.html";
+        } else {
+            joinPlayerInTable(location.href = "gameblackjack.html");
+        }
+
     });
     //callbak al pulsar botón negativo
     confirm.set('oncancel', function () {
--- src/main/resources/static/js/gamesController.js
+++ src/main/resources/static/js/gamesController.js
@@ -12,7 +12,7 @@ var playGame = false;
 var reload = function () {
     listNumbers = [];
     times = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-    
+
     playGame = false;
     loadGame();
 }
@@ -154,5 +154,78 @@ function selectValueCoin(value) {
         }
 
     }
+
+}
+
+function loadTableroHoldEm() {
+    axios.get('/lobbies/' + localStorage.getItem('currentLobbyId') + '/tables/' + localStorage.getItem('currentTable'))
+            .then(function (response) {
+                var table = response.data;
+                document.getElementById("showGame").innerHTML = "<table><tr></tr></table>";
+                var tablero = $("#showGame");
+                var limitPlayers = table['capacity'];
+                var currentPlayers = table['currentPlayers'];
+                console.log(currentPlayers + "/" + limitPlayers);
+                for (var i = 0; i < parseInt(limitPlayers); i++) {
+                    
+                    if (parseInt(currentPlayers) > i) {
+                        //var ancho = (($("#bodyPrincipal").width()/parseInt(currentPlayers))/parseInt(currentPlayers)/parseInt(currentPlayers));
+                        tablero.append( "<tr id='player" + i + "'><td style='padding: 30px;' >"+
+                            "<img src='/img/cartas/card_back.png'/>"+"<img src='/img/cartas/card_01.png'/>"+
+                            "<div id='cards" + i + "'>"+
+                            "<br><br>"+
+                            "<button id='fold" + i + "' style='border:1px solid black;'>Fold</button>"+
+                            "<button id='call" + i + "' style='border:1px solid black;'>Call</button>"+
+                            "<button id='raise" + i + "' style='border:1px solid black;'>Raise To</button>"+
+                            "<button id='allin" + i + "' style='border:1px solid black;'>AllIn</button>"+
+                            "<br><b>Jugador<span id='player" + i + "'></span></b>"+
+                            "</td></tr>");
+                    }
+                    
+                    
+                }
+
+
+            })
+            .catch(function (error) {
+                alert("Error, could not join");
+            })
+    /*document.getElementById("showGame").innerHTML = "<table></table>";
+     var tablero = $("#showGame");
+     var cont = 1;
+     var fila1 = "<tr></tr>";
+     var fila2 = "<tr></tr>";
+     var fila3 = "<tr></tr>";
+     
+     fila2 += ("<td ><button id='number0' style='border:1px solid black;' class='btn btn-success' onclick='numberSelect(0)'>0 <span class='badge'>" + times[0] + "</span> </button></td>");
+     fila3 += ("<td  ><button id='number37' style='border:1px solid black;' class='btn btn-success' onclick='numberSelect(37)'>00 <span class='badge'>" + times[37] + "</span></button></td>");
+     fila1 += ("<td></td>");
+     
+     var listRed = [1, 3, 5, 7, 9, 12, 14, 16, 18, 19, 21, 23, 25, 27, 30, 32, 34, 36];
+     var color = '';
+     
+     for (var i = 1; i < 37; i++) {
+     var onclick = "onclick='numberSelect(" + i + ")'";
+     if (listRed.includes(i) === true) {
+     color = 'btn btn-danger';
+     } else {
+     color = 'btn btn';
+     }
+     if (cont === 1) {
+     fila1 += "<td  ><button id='number" + i + "' style='border:1px solid black;' class='" + color + "' onclick='numberSelect(" + i + ")'>" + i + " <span class='badge'>" + times[i] + "</span> </button></td>";
+     cont += 1;
+     } else if (cont === 2) {
+     fila2 += "<td ><button id='number" + i + "' style='border:1px solid black;' class='" + color + "' onclick='numberSelect(" + i + ")'>" + i + " <span class='badge'>" + times[i] + "</span> </button></td>";
+     cont += 1;
+     } else {
+     fila3 += "<td ><button id='number" + i + "' style='border:1px solid black;' class='" + color + "' onclick='numberSelect(" + i + ")'>" + i + " <span class='badge'>" + times[i] + "</span> </button></td>";
+     cont = 1;
+     }
+     }
+     tablero.append(fila3);
+     tablero.append(fila2);
+     tablero.append(fila1);
+     
+     selectValueCoin(coin);*/
 
 }
